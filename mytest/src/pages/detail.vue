33a3bee19a79bf5a9ff2f4d6588748ae7d5603c1
wangyb <wangyb@i2finance.net>
2018-09-21 17:46:02 +0800

    哈哈我又提交了详情页哦

--- mytest/src/pages/detail.vue
+++ mytest/src/pages/detail.vue
@@ -1,7 +1,18 @@
 <template id="">
-  <div class="container" :click="loadstart()">
-    {{msg}}
-    {{photoId}}
+  <div class="container">
+    <div class="title">{{msg}}</div>
+    <div class="photo_box" @click="openPhoto()">
+      <div class="photo_item">
+        <p class="photo_name vintage2" style="font-size: 3rem;">{{photo.name}}</p>
+        <p class="photo_describe">{{photo.classify}}</p>
+        <p class="photo_data">{{photo.createData}}</p>
+      </div>
+      <ul class="photo_pages">
+        <li :class="{photo_page:true,current:n==index}" v-for="n in photo.page" v-bind:key="n"  @click="changePage(n)">
+          <div class="foot_page">第{{n}}页</div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,15 +22,27 @@ export default {
   data () {
     return {
       msg: '我是详情',
-      photoId: ''
+      photo: JSON.parse(this.$route.query.photo),
+      index: 0
     }
   },
   methods: {
-    loadstart: function () {
-      // console.log(this.$route.query)
-      this.photoId = this.$route.query.photoId
-      // console.log(this.photoId)
+    openPhoto () {
+      console.log(this.index)
+      this.index++
+      console.log(event.currentTarget.childNodes.length)
+    },
+    changePage (n) {
+      console.log(n)
+      if (n >= this.photo.page) {
+        alert('已经最后一页了哦')
+      } else {
+        this.index++
+      }
     }
   }
 }
 </script>
+<style type="text/css" scoped>
+  @import "../assets/css/detail.css";
+</style>
