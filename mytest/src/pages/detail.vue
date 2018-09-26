09a38c41119f5b7aa116ace2dfc55310aed5a9cc
wangyb <wangyb@i2finance.net>
2018-09-26 18:48:12 +0800

    相册详情页翻页效果提交

--- mytest/src/pages/detail.vue
+++ mytest/src/pages/detail.vue
@@ -1,14 +1,15 @@
 <template id="">
   <div class="container">
     <div class="title">{{msg}}</div>
-    <div class="photo_box" @click="openPhoto()">
-      <div class="photo_item">
+    <div class="photo_box">
+      <div class="photo_item" @click="openPhoto()">
         <p class="photo_name vintage2" style="font-size: 3rem;">{{photo.name}}</p>
         <p class="photo_describe">{{photo.classify}}</p>
         <p class="photo_data">{{photo.createData}}</p>
       </div>
-      <ul class="photo_pages">
-        <li :class="{photo_page:true,current:n==index}" v-for="n in photo.page" v-bind:key="n"  @click="changePage(n)">
+      <ul :class="{photo_pages:true,on:open}">
+        <li class="photo_page" :class="{turn_page:index>n,turn_left:index>n,current:index==n}" :style="{'z-index':index>=n?999:999-n}" v-bind:data-pageOpen="0" v-for="n in photo.page" v-bind:key="n"  @click="changePage(n)">
+          <img src="../assets/img/edite/feather4.png"/>
           <div class="foot_page">第{{n}}页</div>
         </li>
       </ul>
@@ -23,23 +24,70 @@ export default {
     return {
       msg: '我是详情',
       photo: JSON.parse(this.$route.query.photo),
-      index: 0
+      index: 0,
+      open: false,
+      timeOut: null,
+      timeOut1: null
     }
   },
   methods: {
+    // 打开关闭封皮
     openPhoto () {
       console.log(this.index)
-      this.index++
-      console.log(event.currentTarget.childNodes.length)
+      let classList = event.currentTarget.classList
+      if (this.open) {
+        classList.add('turn_right')
+        this.open = false
+        this.index = 0
+        this.timeOut1 = setTimeout(() => {
+          classList.remove('turn_none')
+        }, 800)
+      } else {
+        classList.add('current', 'turn_left')
+        classList.remove('turn_right')
+        this.index = 1
+        this.timeOut1 = setTimeout(() => {
+          classList.add('turn_none')
+        }, 800)
+        this.timeOut = setTimeout(() => {
+          this.open = true
+        }, 2000)
+      }
     },
+    // 翻页效果
     changePage (n) {
       console.log(n)
+      console.log(this.index)
+      let _this = event.currentTarget
       if (n >= this.photo.page) {
         alert('已经最后一页了哦')
       } else {
-        this.index++
+        console.log(event.currentTarget.dataset.pageopen)
+        if (event.currentTarget.dataset.pageopen === '1') {
+          event.currentTarget.dataset.pageopen = '0'
+          _this.classList.add('turn_right')
+          _this.style.zIndex = '1000'
+          this.timeOut1 = setTimeout(() => {
+            _this.classList.remove('turn_page')
+          }, 500)
+          this.timeOut = setTimeout(() => {
+            this.index--
+            _this.style.zIndex = 999 - n
+          }, 2000)
+        } else {
+          event.currentTarget.dataset.pageopen = '1'
+          _this.classList.add('turn_left')
+          this.timeOut1 = setTimeout(() => {
+            this.index++
+            _this.classList.add('turn_page')
+          }, 500)
+        }
       }
     }
+  },
+  destroyed () {
+    window.clearTimeout(this.timeOut)
+    window.clearTimeout(this.timeOut1)
   }
 }
 </script>
